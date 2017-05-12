4b3091dd3c7fed6a1be33a73d172b0ac0f4491ad
BradElko <Uvozi@yahoo.com>
2017-05-12 13:35:11 -0400

    Add files via upload

--- PanelWindow/PanelWindow.js
+++ PanelWindow/PanelWindow.js
@@ -1,6 +1,28 @@
 (function(){
-    window.addEventListener("load", OnLoad);
-    function OnLoad(){
-        console.log("hello people");
+    var PanelForm = {}
+    window.addEventListener("load", PanelFormLoaded);
+    document.addEventListener("contextmenu", PanelFormContextMenu);
+    function PanelFormLoaded(){   
+        PanelForm.CreatedContextMenu = false;
+        PanelForm.PanelDivContainer = document.getElementById("container");
+        PanelForm.CMContainer = document.createElement("div");
+        PanelForm.CMMenuPanel = document.createElement("div");
+    }
+    function PanelFormContextMenu(e){
+        if(e.which === 3 || e.keyCode === 3 || e.button === 2){
+            e.preventDefault();
+            if(PanelForm.CreatedContextMenu == false){
+                PanelForm.CMContainer.style.position = "absolute";
+                PanelForm.CMContainer.style.zIndex = 999;
+                PanelForm.CMContainer.style.width = "150px";
+                PanelForm.CMContainer.style.height = "60px";
+                PanelForm.CMContainer.style.border = "1px solid red";
+                PanelForm.CMContainer.appendChild(PanelForm.CMMenuPanel);
+                PanelForm.PanelDivContainer.appendChild(PanelForm.CMContainer);
+                PanelForm.CreatedContextMenu = true;
+            }
+            PanelForm.CMContainer.style.top = e.clientY + "px";
+            PanelForm.CMContainer.style.left = e.clientX + "px";
+        }
     }
 })();
